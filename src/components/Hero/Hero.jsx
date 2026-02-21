55fc64c211c94b84e693c020d6068f39bb51f769
Andrii Nastych <andrii.nastych@gmail.com>
2026-02-21 20:20:24 +0100

    commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,6 +4,12 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="container hero__inner">
+        <p className="hero__subtitle">
+          tel:+33456756578
+        </p>
+          <p className="hero__subtitle">
+          elnara-com@gmail.fr
+        </p>
       </div>
     </section>
   );
